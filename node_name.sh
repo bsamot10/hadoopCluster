2e5d4570d9e04b71a6b8df5f5d63e753eda38573
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-16 17:42:25 +0200

    Create node_name.sh

---
+++ node_name.sh
@@ -0,0 +1,8 @@
+if [ -z $1 ]
+then
+        echo "ERROR: please input the 'node_name' command line argument.";
+        exit 1;
+fi
+
+for file in {run.sh,myid.sh,shell.sh,spark-start-services.sh,jupyter-lab-start.sh,jupyter-lab-stop.sh};
+do sed -i "s/<node_name>/$1/g" $file; done;
