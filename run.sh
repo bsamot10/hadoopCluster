55c8c13b2e25da742806bb06b459ba63401bf050
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-17 16:13:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ docker run -d \
 	--ip "10.0.2.1<node_id>" \
 	-p 9868:9868 \
 	-p 9870:9870 \
+ 	-p 9864:9864 \
 	-p 8088:8088 \
 	-p 8080:8080 \
 	-p 8888:8888 \
 	-p 8047:8047 \
 	-p 16010:16010 \
+ 	-p 16020:16020 \
 	-itd bsamot10/spark-hadoop-cluster:latest
 
 . myid.sh
