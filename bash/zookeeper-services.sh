82d6916959a44ffaa19bf1acecfe964b657868ac
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-18 15:42:44 +0200

    Update zookeeper-services.sh

--- bash/zookeeper-services.sh
+++ bash/zookeeper-services.sh
@@ -2,11 +2,21 @@
 
 $ZOOKEEPER_HOME/bin/zkServer.sh start
 sleep 10
-jps -lm
+echo -e '\n'
+jps
+echo -e '\n'
 
 $DRILL_HOME/bin/drillbit.sh start
 sleep 10
-jps -lm
+echo -e '\n'
+jps
+echo -e '\n'
 
-
-
+if [ $(hostname -f) == 'master' ]
+then
+        $HBASE_HOME/bin/start-hbase.sh
+        sleep 10
+        echo -e '\n'
+        jps
+        echo -e '\n'
+fi
