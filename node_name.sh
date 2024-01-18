cb7e4ed3a6983538c8b85da1c0a7c5c21cb8cebf
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-18 14:16:41 +0200

    Update node_name.sh

--- node_name.sh
+++ node_name.sh
@@ -4,5 +4,5 @@ then
         exit 1;
 fi
 
-for file in {run.sh,myid.sh,shell.sh,spark-start-services.sh,zookeeper-start-services.sh,jupyter-lab-start.sh};
+for file in {run.sh,myid.sh,hbase-site.sh,shell.sh,spark-start-services.sh,zookeeper-start-services.sh,jupyter-lab-start.sh};
 do sed -i "s/<node_name>/$1/g" $file; done;
