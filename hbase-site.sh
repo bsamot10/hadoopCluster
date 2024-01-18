ff05402cc837d8fdd4011343c6e7da7bce3b5599
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-18 14:23:00 +0200

    Update hbase-site.sh

--- hbase-site.sh
+++ hbase-site.sh
@@ -1,1 +1,1 @@
-docker cp <node_name> config/hbase/hbase-site-worker.xml  /usr/local/hbase/conf/hbase-site.xml
+docker cp config/hbase/hbase-site-worker.xml  <node_name>:/usr/local/hbase/conf/hbase-site.xml
